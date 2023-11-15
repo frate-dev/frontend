84a350f41efa2245e04ac83930dc13f56ee940d8
lvcky-gg <matadorcrab@gmail.com>
2023-11-15 09:38:13 -0600

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { useDispatch, useSelector } from 'react-redux';
-import { filterByStars, filterForWord, getAllPackages } from './store/packages';
-import Header from "./components/header/index"
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { filterByStars, filterForWord, getAllPackages } from "./store/packages";
+import Header from "./components/header/index";
 
 function App() {
   const [count, setCount] = useState(0);
   const dispatch = useDispatch();
-  const packages = useSelector((state)=>state.packages.packages);
-  useEffect(()=>{
-    dispatch(getAllPackages())
+  const packages = useSelector((state) => state.packages.packages);
+  useEffect(() => {
+    dispatch(getAllPackages());
     // dispatch(filterForWord("7bitconf"))
-    
-  },[dispatch])
-  
-
-
-
+  }, [dispatch]);
 
   return (
     <>
-    <Header></Header>
- 
+      <Header></Header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     navigate("/");
   };
   return (
-    <div className="flex justify-between pl-4 bg-[#f7f7f7]">
+    <div className="flex justify-between pl-4 bg-[#f7f7f7] h-32">
       <div className="flex">
         <img
           onClick={handleHome}
@@ -16,9 +16,9 @@ export default function Header() {
           src="https://raw.githubusercontent.com/cmaker-dev/cmaker/6c64104821ca7f9e17583ecf30c7d2ac225a9b58/source/images/cmakerlogo.svg"
           alt="cmakeLogo"
         />
-        <h1 className="h-8 w-20 mt-8 ml-4 text-4xl text-gray">Index</h1>
+        <h1 className="h-8 w-20 mt-12 ml-4 text-4xl text-gray">Index</h1>
       </div>
-      <button className="mr-20 bg-blue h-10 w-40 mt-8 rounded-md hover:bg-green">
+      <button className="mr-20 bg-blue h-10 w-40 mt-12 rounded-md hover:bg-green">
         {" "}
         Submit Package
       </button>
