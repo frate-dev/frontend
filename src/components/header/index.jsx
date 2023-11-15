c88a9d69ab56fca5cdec8017325e0630aa612f4e
lvcky-gg <matadorcrab@gmail.com>
2023-11-15 08:54:32 -0600

    added the header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,22 +1,20 @@
-import * as React from 'react';
-import "./header.css";
-
+import * as React from "react";
 
 export default function Header() {
-
-
-
-
- 
-
   return (
-    <div id='header-contatiner'>
-      <div id='header-box'>
-      <img id='header-logo' src="https://raw.githubusercontent.com/cmaker-dev/cmaker/6c64104821ca7f9e17583ecf30c7d2ac225a9b58/source/images/cmakerlogo.svg" 
-      alt="cmakeLogo"/>
-      <h1>Index</h1>
+    <div class="flex justify-between pl-4 bg-[#f7f7f7]">
+      <div class="flex">
+        <img
+          class="w-24"
+          src="https://raw.githubusercontent.com/cmaker-dev/cmaker/6c64104821ca7f9e17583ecf30c7d2ac225a9b58/source/images/cmakerlogo.svg"
+          alt="cmakeLogo"
+        />
+        <h1 class="h-8 w-20 mt-8 ml-8 text-4xl text-gray">Index</h1>
       </div>
-      <button>Submit Package</button>
+      <button class="mr-20 bg-blue h-10 w-40 mt-8 rounded-md">
+        {" "}
+        Submit Package
+      </button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#064F8C",
+      lightblue: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#CDCDCE",
+    },
+    fontFamily: {
+      sans: ["roboto", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
 };
