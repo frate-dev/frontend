a3dd02db3199ac8a5fc6b04395639db6b0b605f4
lvcky-gg <matadorcrab@gmail.com>
2023-11-15 08:55:11 -0600

    some modification

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -9,7 +9,7 @@ export default function Header() {
           src="https://raw.githubusercontent.com/cmaker-dev/cmaker/6c64104821ca7f9e17583ecf30c7d2ac225a9b58/source/images/cmakerlogo.svg"
           alt="cmakeLogo"
         />
-        <h1 class="h-8 w-20 mt-8 ml-8 text-4xl text-gray">Index</h1>
+        <h1 class="h-8 w-20 mt-8 ml-4 text-4xl text-gray">Index</h1>
       </div>
       <button class="mr-20 bg-blue h-10 w-40 mt-8 rounded-md">
         {" "}
