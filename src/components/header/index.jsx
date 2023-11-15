fc63f9b438725253c8187784e4acf1eb72e1e11e
lvcky-gg <matadorcrab@gmail.com>
2023-11-15 09:17:21 -0600

    added routing

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,17 +1,24 @@
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const handleHome = (e) => {
+    // e.preventdefault();
+    navigate("/");
+  };
   return (
-    <div class="flex justify-between pl-4 bg-[#f7f7f7]">
-      <div class="flex">
+    <div className="flex justify-between pl-4 bg-[#f7f7f7]">
+      <div className="flex">
         <img
-          class="w-24"
+          onClick={handleHome}
+          className="w-24"
           src="https://raw.githubusercontent.com/cmaker-dev/cmaker/6c64104821ca7f9e17583ecf30c7d2ac225a9b58/source/images/cmakerlogo.svg"
           alt="cmakeLogo"
         />
-        <h1 class="h-8 w-20 mt-8 ml-4 text-4xl text-gray">Index</h1>
+        <h1 className="h-8 w-20 mt-8 ml-4 text-4xl text-gray">Index</h1>
       </div>
-      <button class="mr-20 bg-blue h-10 w-40 mt-8 rounded-md">
+      <button className="mr-20 bg-blue h-10 w-40 mt-8 rounded-md">
         {" "}
         Submit Package
       </button>
