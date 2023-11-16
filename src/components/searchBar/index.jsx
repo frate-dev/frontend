b11f3bea664df8196940dcdf95982bc5ecbf2443
John O'Donnell <matadorcrab@gmail.com>
2023-11-16 16:57:13 -0600

    started the search bar

--- src/components/searchBar/index.jsx
+++ src/components/searchBar/index.jsx
@@ -4,6 +4,16 @@ import { useNavigate } from "react-router-dom";
 
 export default function SearchBar(){
     const [url, setUrl] = useState("");
+    const navigate = useNavigate();
+
+    const handelSearch = e =>{
+        e.preventDefault()
+        if(url != ""){
+            navigate(`/packages${url}`)
+        }else{
+            navigate("/packages")
+        }
+    }
     return (<>
     </>)
 }
