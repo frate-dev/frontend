f48223e9b575f846829230ac7fc261e1a3400833
John O'Donnell <matadorcrab@gmail.com>
2023-11-17 23:36:50 -0600

    sad panda

--- src/pages/packages/index.jsx
+++ src/pages/packages/index.jsx
@@ -20,7 +20,9 @@ export default function PackagesPage(){
     return (
         <div>
             {
-                displayedItems.length ? (<div>yes</div>):<div>No</div>
+                displayedItems.length ? (<div>yes</div>):
+                (<div>
+                    <img src="https://media1.giphy.com/media/7p3e2WCM0VEnm/giphy.gif?cid=ecf05e47li5v9zdago8wibx14hdy8jl7ixqeoa01hv1m5zx8&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="sad panda"/></div>)
             }
             
         </div>
