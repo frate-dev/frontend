0f35fb3d79ceed0a42119dd57b5173ca7066de00
Douglas Phillips <douglasphillips.eng@gmail.com>
2023-11-20 12:29:04 -0600

    Rework package index search

--- src/pages/packages/index.jsx
+++ src/pages/packages/index.jsx
@@ -1,97 +1,123 @@
+import { useCallback, useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { filterForWord, getAllPackages } from "../../store/packages";
-import { useEffect } from "react";
+import { getAllPackages, filterForWord } from "../../store/packages";
 import { useSearchParams } from "react-router-dom";
+
+const ITEMS_PER_PAGE_DEFAULT = 50;
 
 export default function PackagesPage() {
   const [searchParams, setSearchParams] = useSearchParams();
   const dispatch = useDispatch();
-  const displayedItems = useSelector((state) => state.packages.displayPackages);
+  const displayedItems = useSelector(
+    (state) => state.packages.displayPackages || []
+  );
 
+  const query = searchParams.get("query");
   const currentPage = parseInt(searchParams.get("page") || "1", 10);
-  const itemsPerPage = parseInt(searchParams.get("size") || "50", 10);
+  const itemsPerPage = parseInt(
+    searchParams.get("size") || ITEMS_PER_PAGE_DEFAULT.toString(),
+    10
+  );
 
   useEffect(() => {
-    dispatch(getAllPackages());
-    const query = searchParams.get("query");
-    const from = (currentPage - 1) * itemsPerPage;
     if (query) {
+      dispatch(getAllPackages());
+      const from = (currentPage - 1) * itemsPerPage;
       dispatch(filterForWord(query, from, itemsPerPage));
     }
-  }, [dispatch, searchParams, currentPage, itemsPerPage]);
+  }, [dispatch, query, currentPage, itemsPerPage]);
+
+  const capitalizeWords = useCallback(
+    (str) => str.replace(/\b\w/g, (char) => char.toUpperCase()),
+    []
+  );
 
   const totalItems = displayedItems.length;
   const totalPages = Math.ceil(totalItems / itemsPerPage);
   const firstItemIndex = (currentPage - 1) * itemsPerPage;
   const lastItemIndex = firstItemIndex + itemsPerPage;
-  const currentItems = displayedItems.slice(firstItemIndex, lastItemIndex);
+  const currentItems = useMemo(
+    () => displayedItems.slice(firstItemIndex, lastItemIndex),
+    [firstItemIndex, lastItemIndex, displayedItems]
+  );
 
-  const updatePageParam = (newPage) => {
-    const newSearchParams = new URLSearchParams(searchParams);
-    newSearchParams.set("page", newPage.toString());
-    if (itemsPerPage !== 50) {
-      newSearchParams.set("size", itemsPerPage.toString());
-    }
-    setSearchParams(newSearchParams);
-  };
+  const updatePageParam = useCallback(
+    (newPage) => {
+      const newSearchParams = new URLSearchParams(searchParams);
+      newSearchParams.set("page", newPage.toString());
+      if (itemsPerPage !== ITEMS_PER_PAGE_DEFAULT) {
+        newSearchParams.set("size", itemsPerPage.toString());
+      }
+      setSearchParams(newSearchParams);
+    },
+    [searchParams, itemsPerPage, setSearchParams]
+  );
 
-  const goToNextPage = () => {
+  const goToNextPage = useCallback(() => {
     if (currentPage < totalPages) {
       updatePageParam(currentPage + 1);
     }
-  };
+  }, [currentPage, totalPages, updatePageParam]);
 
-  const goToPreviousPage = () => {
+  const goToPreviousPage = useCallback(() => {
     if (currentPage > 1) {
       updatePageParam(currentPage - 1);
     }
-  };
+  }, [currentPage, updatePageParam]);
 
-  const renderCards = () =>
-    currentItems.map((packageItem, index) => (
-      <div
-        key={index}
-        className="bg-white border border-[#C8C8C8] p-4 mb-4 rounded flex justify-between items-center w-full"
-      >
-        <div className="w-1/2">
-          <h5 className="text-2xl font-bold mb-2 underline">
-            {packageItem.name}
-          </h5>
-          <p>{packageItem.description}</p>
+  const renderCards = useMemo(
+    () =>
+      currentItems.map((packageItem, index) => (
+        <div
+          key={packageItem.id || index}
+          className="bg-white border border-[#C8C8C8] p-4 mb-4 rounded flex justify-between items-center w-full"
+        >
+          <div className="w-1/2">
+            <h5 className="text-2xl font-bold mb-2 underline">
+              {capitalizeWords(packageItem.name)}
+            </h5>
+            <p>{packageItem.description}</p>
+          </div>
+          <div className="flex flex-col items-end">
+            <span className="text-sm font-semibold">{packageItem.version}</span>
+          </div>
         </div>
-        <div className="flex flex-col items-end">
-          <span className="text-sm font-semibold">{packageItem.version}</span>
-        </div>
-      </div>
-    ));
+      )),
+    [currentItems, capitalizeWords]
+  );
 
   return (
     <div className="w-1/2 h-full mx-auto my-20">
-      <div className="mb-4">
-        Showing results {firstItemIndex + 1}-
-        {Math.min(lastItemIndex, totalItems)} of {totalItems} packages.
-      </div>
-      <div className="flex flex-col items-center">{renderCards()}</div>
-      {totalItems > itemsPerPage && (
-        <div className="flex justify-between items-center my-4">
-          <button
-            onClick={goToPreviousPage}
-            disabled={currentPage === 1}
-            className="disabled:opacity-50 disabled:cursor-not-allowed"
-          >
-            Previous
-          </button>
-          <span>
-            Page {currentPage} of {totalPages}
-          </span>
-          <button
-            onClick={goToNextPage}
-            disabled={currentPage === totalPages}
-            className="disabled:opacity-50 disabled:cursor-not-allowed"
-          >
-            Next
-          </button>
-        </div>
+      {query && (
+        <>
+          <div className="mb-4">
+            Showing results {firstItemIndex + 1}-
+            {Math.min(lastItemIndex, totalItems)} of {totalItems} packages for{" "}
+            {query}.
+          </div>
+          <div className="flex flex-col items-center">{renderCards}</div>
+          {totalItems > itemsPerPage && (
+            <div className="flex justify-between items-center my-4">
+              <button
+                onClick={goToPreviousPage}
+                disabled={currentPage === 1}
+                className="disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                Previous
+              </button>
+              <span>
+                Page {currentPage} of {totalPages}
+              </span>
+              <button
+                onClick={goToNextPage}
+                disabled={currentPage === totalPages}
+                className="disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                Next
+              </button>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
